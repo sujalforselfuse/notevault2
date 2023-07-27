c7081b94cdce18968cfcf7ba4cce0c2555960a15
sujalforselfuse <110181297+sujalforselfuse@users.noreply.github.com>
2023-07-28 01:20:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,1 @@
-/* const mongoose=require("mongoose");
 
-const URI="mongodb+srv://sujalforselfuse:U9WYi630W56XiJXi@cluster0.sd7lwxg.mongodb.net/inotebook?retryWrites=true&w=majority";
-
-const connecttomongo=()=>{
-    mongoose.connect(URI);
-    console.log("db connected");
-}
-
-module.exports=connecttomongo;
-
-
-
- */
